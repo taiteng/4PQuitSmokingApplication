90b4ac8d0b6708ff6febe465941173567a1993d2
esoon <p20012522@student.newinti.edu.my>
2022-05-23 11:55:15 +0800

    Title Change

--- lib/main.dart
+++ lib/main.dart
@@ -24,7 +24,7 @@ class MyApp extends StatelessWidget {
         // is not restarted.
         primarySwatch: Colors.green,
       ),
-      home: const MyHomePage(title: 'Flutter Demo Home Page'),
+      home: const MyHomePage(title: 'Quit Smoking App'),
     );
   }
 }

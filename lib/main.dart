61cd4bd3dc930d9217bca08672ebed67f1468cc1
Lee-ZC <leezc0631@gmail.com>
2022-06-20 13:53:10 +0800

    Home page

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,7 @@
+import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
+import 'package:lottie/lottie.dart';
+import 'NavBar.dart';
 
 void main() {
   runApp(const MyApp());
@@ -7,39 +10,27 @@ void main() {
 class MyApp extends StatelessWidget {
   const MyApp({Key? key}) : super(key: key);
 
-  // This widget is the root of your application.
+
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Flutter Demo',
+      title: 'Side Bar',
       theme: ThemeData(
-        // This is the theme of your application.
-        //
-        // Try running your application with "flutter run". You'll see the
-        // application has a blue toolbar. Then, without quitting the app, try
-        // changing the primarySwatch below to Colors.green and then invoke
-        // "hot reload" (press "r" in the console where you ran "flutter run",
-        // or simply save your changes to "hot reload" in a Flutter IDE).
-        // Notice that the counter didn't reset back to zero; the application
-        // is not restarted.
-        primarySwatch: Colors.green,
+        primarySwatch: Colors.blue,
       ),
-      home: const MyHomePage(title: 'Flutter Demo Home Page'),
+      home: const MyHomePage(title: 'Tween'),
     );
   }
+
+
 }
+
+
+
+
 
 class MyHomePage extends StatefulWidget {
   const MyHomePage({Key? key, required this.title}) : super(key: key);
-
-  // This widget is the home page of your application. It is stateful, meaning
-  // that it has a State object (defined below) that contains fields that affect
-  // how it looks.
-
-  // This class is the configuration for the state. It holds the values (in this
-  // case the title) provided by the parent (in this case the App widget) and
-  // used by the build method of the State. Fields in a Widget subclass are
-  // always marked "final".
 
   final String title;
 
@@ -47,69 +38,374 @@ class MyHomePage extends StatefulWidget {
   State<MyHomePage> createState() => _MyHomePageState();
 }
 
-class _MyHomePageState extends State<MyHomePage> {
-  int _counter = 0;
 
-  void _incrementCounter() {
-    setState(() {
-      // This call to setState tells the Flutter framework that something has
-      // changed in this State, which causes it to rerun the build method below
-      // so that the display can reflect the updated values. If we changed
-      // _counter without calling setState(), then the build method would not be
-      // called again, and so nothing would appear to happen.
-      _counter++;
-    });
-  }
+
+
+
+class _MyHomePageState extends State<MyHomePage> {
 
   @override
   Widget build(BuildContext context) {
-    // This method is rerun every time setState is called, for instance as done
-    // by the _incrementCounter method above.
-    //
-    // The Flutter framework has been optimized to make rerunning build methods
-    // fast, so that you can just rebuild anything that needs updating rather
-    // than having to individually change instances of widgets.
+
     return Scaffold(
+      backgroundColor: Colors.grey[500],
+      drawer:  NavBar(),
       appBar: AppBar(
-        // Here we take the value from the MyHomePage object that was created by
-        // the App.build method, and use it to set our appbar title.
+        backgroundColor: Colors.red[600],
         title: Text(widget.title),
+        centerTitle: true,
+
       ),
-      body: Center(
-        // Center is a layout widget. It takes a single child and positions it
-        // in the middle of the parent.
+
+
+
+
+      body: SingleChildScrollView(
+
         child: Column(
-          // Column is also a layout widget. It takes a list of children and
-          // arranges them vertically. By default, it sizes itself to fit its
-          // children horizontally, and tries to be as tall as its parent.
-          //
-          // Invoke "debug painting" (press "p" in the console, choose the
-          // "Toggle Debug Paint" action from the Flutter Inspector in Android
-          // Studio, or the "Toggle Debug Paint" command in Visual Studio Code)
-          // to see the wireframe for each widget.
-          //
-          // Column has various properties to control how it sizes itself and
-          // how it positions its children. Here we use mainAxisAlignment to
-          // center the children vertically; the main axis here is the vertical
-          // axis because Columns are vertical (the cross axis would be
-          // horizontal).
-          mainAxisAlignment: MainAxisAlignment.center,
           children: <Widget>[
-            const Text(
-              'You have pushed the button this many times:',
+
+              Container(
+                color: Colors.lightBlue,
+                width: 420,
+                height: 100,
+
+                child: Row(
+                  crossAxisAlignment: CrossAxisAlignment.start,
+                  mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+
+                    children: <Widget>[
+                      Text('  Money Saved \n\n \t\t\t\t\t\t\t\t\t 0',
+                        style: TextStyle(
+                          fontSize: 20,
+                          fontWeight: FontWeight. bold,
+                        ),
+                      ),
+                      Lottie.network('https://assets8.lottiefiles.com/packages/lf20_i0mxtka6.json',width: 110,height: 110),
+                      Text('Total Achivement \n\n \t\t\t\t\t\t\t\t\t\t\t\t\t 0',
+                        style: TextStyle(
+                          fontSize: 20,
+                          fontWeight: FontWeight. bold,
+                        ),
+                      ),
+                  ],
+                ),
+              ),
+
+            SizedBox(height: 20),
+
+            Container(
+              color: Colors.purple,
+              width: 420,
+              height: 100,
+
+              child: Row(
+
+                children: <Widget>[
+
+                  Text('    Life Won \n\n \t\t\t\t\t\t 0',
+                    style: TextStyle(
+                      fontSize: 20,
+                      fontWeight: FontWeight. bold,
+                    ),
+                  ),
+                  Text('\t\t\t\t\t\t\t\t\t\t\t\t\t\t\t\t\t\t\t\t\t\t\t\t\t\t\tCigarettes Ignored \n\n \t\t\t\t\t\t\t\t\t\t\t\t\t\t\t\t\t\t\t\t\t\t\t\t\t\t\t\t\t\t\t\t\t\t\t\t\t\t\t\t 0',
+                    style: TextStyle(
+                      fontSize: 20,
+                      fontWeight: FontWeight. bold,
+                    ),
+                  ),
+                ],
+              ),
             ),
-            Text(
-              '$_counter',
-              style: Theme.of(context).textTheme.headline4,
+
+
+            SizedBox(height: 20),
+
+            Container(
+              color: Colors.yellow,
+              width: 420,
+              height: 50,
+
+              child: Row(
+                mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                children: <Widget>[
+                    Text('You have stopped smoking for',
+                      style: TextStyle(
+                        fontSize: 20,
+                        fontWeight: FontWeight. bold,
+                      ),
+                    ),
+                ],
+              ),
             ),
+
+
+            Container(
+              color: Colors.yellow,
+              width: 420,
+              height: 50,
+
+              child: Row(
+
+                mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+
+
+                children: <Widget>[
+                  Text('0',textAlign: TextAlign.center,
+                    style: TextStyle(
+                      fontSize: 20,
+                      fontWeight: FontWeight. bold,
+                    ),
+                  ),
+                  Text('0',textAlign: TextAlign.center,
+                    style: TextStyle(
+                      fontSize: 20,
+                      fontWeight: FontWeight. bold,
+                    ),
+                  ),
+                  Text('0',textAlign: TextAlign.center,
+                    style: TextStyle(
+                      fontSize: 20,
+                      fontWeight: FontWeight. bold,
+                    ),
+                  ),
+
+                ],
+              ),
+            ),
+
+
+            Container(
+              color: Colors.yellow,
+              width: 420,
+              height: 50,
+
+              child: Row(
+
+                crossAxisAlignment: CrossAxisAlignment.start,
+                mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+
+
+                children: <Widget>[
+                  Text('     Days',textAlign: TextAlign.center,
+                    style: TextStyle(
+                      fontSize: 20,
+                      fontWeight: FontWeight. bold,
+                    ),
+                  ),
+                  Text('Hours',textAlign: TextAlign.center,
+                    style: TextStyle(
+                      fontSize: 20,
+                      fontWeight: FontWeight. bold,
+                    ),
+                  ),
+                  Text('Minutes',textAlign: TextAlign.center,
+                    style: TextStyle(
+                      fontSize: 20,
+                      fontWeight: FontWeight. bold,
+                    ),
+                  ),
+                  Icon(Icons.share),
+                ],
+              ),
+            ),
+
+            SizedBox(height: 20),
+
+            Container(
+              color: Colors.lightGreenAccent,
+              width: 420,
+              height: 25,
+
+
+              child:Row(
+                mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+
+                children: <Widget>[
+                  Text('Achivement              \n\n \t\t\t\t\t\t\t\t\t',
+                    style: TextStyle(
+                      fontSize: 20,
+                      fontWeight: FontWeight. bold,
+                    ),
+                  ),
+                  Text('                 See More \n\n',
+                    style: TextStyle(
+                      fontSize: 20,
+                      fontWeight: FontWeight. bold,
+                    ),
+                  ),
+                ],
+              ),
+
+            ),
+
+
+            Container(
+              color: Colors.lightGreenAccent,
+              width: 420,
+              height: 50,
+              child:Row(
+                mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+
+                children: <Widget>[
+                  Text('XXXXXXXXXXXXXXXXXXXXXXXXX',
+                    style: TextStyle(
+                      fontSize: 20,
+                      backgroundColor: Colors.red[100]
+                    ),
+                  ),
+                  Icon(Icons.share),
+                ],
+              ),
+            ),
+
+            Container(
+              color: Colors.lightGreenAccent,
+              width: 420,
+              height: 50,
+              child:Row(
+                mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+
+                children: <Widget>[
+                  Text('XXXXXXXXXXXXXXXXXXXXXXXXX',
+                    style: TextStyle(
+                        fontSize: 20,
+                        backgroundColor: Colors.red[100]
+                    ),
+                  ),
+                  Icon(Icons.share),
+                ],
+              ),
+            ),
+
+            Container(
+              color: Colors.lightGreenAccent,
+              width: 420,
+              height: 50,
+              child:Row(
+                mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+
+                children: <Widget>[
+                  Text('XXXXXXXXXXXXXXXXXXXXXXXXX',
+                    style: TextStyle(
+                        fontSize: 20,
+                        backgroundColor: Colors.red[100]
+                    ),
+                  ),
+                  Icon(Icons.share),
+                ],
+              ),
+            ),
+
+
+            SizedBox(height: 20),
+
+
+            Container(
+              color: Colors.pink,
+              width: 420,
+              height: 25,
+
+
+              child:Row(
+                crossAxisAlignment: CrossAxisAlignment.start,
+                mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+
+                children: <Widget>[
+                  Text('Pro Tips                     \n\n \t\t\t\t\t\t\t\t\t',
+                    style: TextStyle(
+                      fontSize: 20,
+                      fontWeight: FontWeight. bold,
+                    ),
+                  ),
+                  Text('                 See More \n\n',
+                    style: TextStyle(
+                      fontSize: 20,
+                      fontWeight: FontWeight. bold,
+                    ),
+                  ),
+                ],
+              ),
+            ),
+
+
+            Container(
+              color: Colors.pink,
+              width: 420,
+              height: 50,
+
+
+              child:Row(
+
+                mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+
+                children: <Widget>[
+                  Text('XXXXXXXXXXXXXXXXXXXXXXXXXX',
+                    style: TextStyle(
+                      fontSize: 20,
+                    ),
+                  ),
+                  Icon(Icons.share),
+                ],
+              ),
+            ),
+
+
+            SizedBox(height: 20),
+
+
+            Container(
+              color: Colors.orange,
+              width: 420,
+              height: 150,
+
+              child: Row(
+                children: <Widget>[
+                  Lottie.network('https://assets4.lottiefiles.com/packages/lf20_ik4jyixs.json',width: 110,height: 110),
+                  Text('\t\t Community\n\n \t Name  \n\n \t XXXXXXXXXXXXXX',
+                    style: TextStyle(
+                      fontSize: 20,
+                      fontWeight: FontWeight. bold,
+                    ),
+                  ),
+
+                ],
+              ),
+            ),
+
+
+            SizedBox(height: 20),
+
+            Container(
+              color: Colors.green,
+              width: 420,
+              height: 80,
+
+              child: Row(
+                mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                children: <Widget>[
+                  Text('Google Ads Banner',
+                    style: TextStyle(
+                      fontSize: 20,
+                      fontWeight: FontWeight. bold,
+                    ),
+                  ),
+                ],
+              ),
+            ),
+
+
+
+
+
+
           ],
         ),
+
+
+
       ),
-      floatingActionButton: FloatingActionButton(
-        onPressed: _incrementCounter,
-        tooltip: 'Increment',
-        child: const Icon(Icons.add),
-      ), // This trailing comma makes auto-formatting nicer for build methods.
     );
   }
 }

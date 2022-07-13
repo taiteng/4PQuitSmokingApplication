200de143feece4179f0aab3b45ab731ebd21c2fc
taite <taiengchan@gmail.com>
2022-07-13 11:33:24 +0800

    testing

--- lib/authenticationService.dart
+++ lib/authenticationService.dart
@@ -3,7 +3,6 @@ import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_facebook_auth/flutter_facebook_auth.dart';
 
-
 class authenticationService{
   final FirebaseAuth _firebaseAuth;
 

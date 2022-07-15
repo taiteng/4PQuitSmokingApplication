990feb9fb3ba4a25927798d1c2ccfd1982b58cea
taite <taiengchan@gmail.com>
2022-07-15 10:32:23 +0800

    fixed achievement

--- lib/userInfo.dart
+++ lib/userInfo.dart
@@ -4,31 +4,55 @@ import 'package:firebase_core/firebase_core.dart';
 import 'package:cloud_firestore/cloud_firestore.dart';
 
 class getUserInfo{
+  final User? user = FirebaseAuth.instance.currentUser;
+
   String? getUID(){
-    final User? user = FirebaseAuth.instance.currentUser;
     final String? uid = user?.uid.toString();
 
     return uid;
   }
 
   String? getUName(){
-    final User? user = FirebaseAuth.instance.currentUser;
     final String? uname = user?.displayName.toString();
 
     return uname;
   }
 
   String? getUImg(){
-    final User? user = FirebaseAuth.instance.currentUser;
     final String? uimg = user?.photoURL.toString();
 
     return uimg;
   }
 
   String? getUEmail(){
-    final User? user = FirebaseAuth.instance.currentUser;
     final String? uemail = user?.email.toString();
 
     return uemail;
+  }
+
+  bool getIsPro(){
+    //bool isPro = FirebaseFirestore.instance.collection('users').doc(user?.uid.toString()).collection('isPro').get() as bool;
+    //final bool? isPro = user?.isPro;
+
+    // if(isPro == true){
+    //   return true;
+    // }
+    // else{
+    //   return false;
+    // }
+    return true;
+  }
+
+  bool getIsAdmin(){
+    //bool isAdmin = FirebaseFirestore.instance.collection('users').doc(user?.uid.toString()).collection('isAdmin').get() as bool;
+    //final bool? isAdmin = user?.isAdmin;
+
+    // if(isAdmin == true){
+    //   return true;
+    // }
+    // else{
+    //   return false;
+    // }
+    return true;
   }
 }

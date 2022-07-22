277e6e8cf13e5d5a75de6b04914155d878d6ed74
Lee-ZC <leezc0631@gmail.com>
2022-07-22 14:17:11 +0800

    HOME (DONE)

--- lib/main.dart
+++ lib/main.dart
@@ -1,11 +1,21 @@
+import 'dart:async';
+
 import 'package:cloud_firestore/cloud_firestore.dart';
+import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 import 'NavBar.dart';
 import 'package:firebase_core/firebase_core.dart';
+import 'package:google_mobile_ads/google_mobile_ads.dart';
+import 'protips.dart';
+import 'package:share_plus/share_plus.dart';
+
+
+
 
 Future main() async{
   WidgetsFlutterBinding.ensureInitialized();
+  MobileAds.instance.initialize();
   await Firebase.initializeApp();
   runApp(const MyApp());
 }
@@ -48,9 +58,102 @@ class MyHomePage extends StatefulWidget {
 
 class _MyHomePageState extends State<MyHomePage> {
 
+  final BannerAd myBanner = BannerAd(
+
+    adUnitId: 'ca-app-pub-3940256099942544/6300978111',
+    size: AdSize.banner,
+    request: AdRequest(),
+    listener: BannerAdListener(
+      // Called when an ad is successfully received.
+      onAdLoaded: (Ad ad) => print('Ad loaded.'),
+      // Called when an ad request failed.
+      onAdFailedToLoad: (Ad ad, LoadAdError error) {
+        // Dispose the ad here to free resources.
+        ad.dispose();
+        print('Ad failed to load: $error');
+      },
+      // Called when an ad opens an overlay that covers the screen.
+      onAdOpened: (Ad ad) => print('Ad opened.'),
+      // Called when an ad removes an overlay that covers the screen.
+      onAdClosed: (Ad ad) => print('Ad closed.'),
+      // Called when an impression occurs on the ad.
+      onAdImpression: (Ad ad) => print('Ad impression.'),
+    ),
+
+  );
+
+
+
+  Future  <void> quitSmokingDate(String days, String hours,String minutes , String seconds) async{
+
+    String time = days + ":" + hours + ":" + minutes + ":" + seconds;
+
+    final User? user = FirebaseAuth.instance.currentUser;
+    final String? uid = user?.uid.toString();
+    final surveyQuestion = FirebaseFirestore.instance.collection('surveys').doc('z9KAl1swgqbtdk1BOEMPIUVVRyz1');
+    await surveyQuestion.update({"time": time});
+  }
+
+
+
+  Future  <String> getDays() async{
+    DocumentSnapshot snapshot = await(FirebaseFirestore.instance.collection('surveys').doc('z9KAl1swgqbtdk1BOEMPIUVVRyz1').get());
+    String day = snapshot['time'];
+    return day.toString();
+  }
+
+
+
+
 
   final CollectionReference _surveyss =
   FirebaseFirestore.instance.collection('surveys');
+
+
+  static const countdownDuration = Duration(seconds: 10);
+  Duration duration = Duration();
+  Timer? timer ;
+  bool isCountdown = false;
+
+  @override
+  void initState() {
+
+    super.initState();
+    startTimer();
+    reset();
+    stopTimer();
+    myBanner.load();
+  }
+
+  void reset(){
+    if(isCountdown){
+      setState(()=> duration = countdownDuration);
+    }
+    else{
+      setState(()=> duration= Duration());
+    }
+  }
+
+  void stopTimer({bool resets = true}){
+    if(resets){
+      reset();
+    }
+  }
+
+  void addTime(){
+    final addSeconds = 1 ;
+
+    setState(() {
+      final seconds = duration.inSeconds + addSeconds;
+
+      duration = Duration(seconds: seconds);
+    });
+
+  }
+
+  void startTimer(){
+    timer = Timer.periodic(Duration(seconds:1),(_)=>addTime());
+  }
 
 
   @override
@@ -62,6 +165,7 @@ class _MyHomePageState extends State<MyHomePage> {
         centerTitle: true,
         title: const Text('Tween'),
       ),
+
       // Using StreamBuilder to display all surveys from Firestore in real-time
       body: StreamBuilder(
         stream: _surveyss.snapshots(),
@@ -72,6 +176,32 @@ class _MyHomePageState extends State<MyHomePage> {
               itemBuilder: (context, index) {
                 final DocumentSnapshot documentSnapshot =
                 streamSnapshot.data!.docs[index];
+
+                var cpd = documentSnapshot['cigarattesPerDay'].toString();
+                var cpp = documentSnapshot['costPerPack'].toString();
+                var qpp = documentSnapshot['quantityPerPack'].toString();
+
+
+                var cigarattesPerDay = int.parse(cpd);
+                var costPerPack = int.parse(cpp);
+                var quantityPerPack = int.parse(qpp);
+
+
+                var moneysaved = ((costPerPack/quantityPerPack)*cigarattesPerDay).toString() ;
+                var lifewon = (cigarattesPerDay*11).toString();
+
+
+                String twoDigits(int n ) => n.toString().padLeft(2,'0');
+                final days = twoDigits(duration.inDays);
+                final hours = twoDigits(duration.inHours);
+                final minutes = twoDigits(duration.inMinutes.remainder(60));
+                final seconds = twoDigits(duration.inSeconds.remainder(60));
+
+                int updated = 0;
+
+                if (updated==0) {
+                  quitSmokingDate(days, hours, minutes, seconds);
+                }
 
                 return Card(
                   child: Column(
@@ -86,8 +216,8 @@ class _MyHomePageState extends State<MyHomePage> {
                               children: <Widget>[
                                 Column(
                                   children: <Widget>[
-                                    Text(documentSnapshot['cigarattesPerDay'].toString(), style: TextStyle(fontSize: 60)),
-                                    Text("Saved", style: TextStyle(fontSize: 20)),
+                                    Text("RM"+ moneysaved , style: TextStyle(fontSize: 60)),
+                                    Text("Money Saved", style: TextStyle(fontSize: 20)),
                                   ],
                                 ),
                                 Container(
@@ -99,8 +229,8 @@ class _MyHomePageState extends State<MyHomePage> {
                                 ),
                                 Column(
                                   children: <Widget>[
-                                    Text(documentSnapshot['costPerPack'].toString(), style: TextStyle(fontSize: 60)),
-                                    Text("Cost", style: TextStyle(fontSize: 20)),
+                                    Text(lifewon + "min",style: TextStyle(fontSize: 60)),
+                                    Text("Life Won Back", style: TextStyle(fontSize: 20)),
                                   ],
                                 ),
                               ],
@@ -109,30 +239,18 @@ class _MyHomePageState extends State<MyHomePage> {
                         ),
                         SizedBox(height: 5),
                         Container(
-                          color: Colors.red,
+                          color: Colors.yellow,
                           child: Padding(
                             padding: const EdgeInsets.only(top: 12.0, bottom: 12.0),
 
                             child: Row(
                               mainAxisAlignment: MainAxisAlignment.spaceEvenly,
                               children: <Widget>[
+
                                 Column(
                                   children: <Widget>[
                                     Text(documentSnapshot['quantityPerPack'].toString(), style: TextStyle(fontSize: 60)),
-                                    Text("Saved", style: TextStyle(fontSize: 20)),
-                                  ],
-                                ),
-                                Container(
-                                  height: 80,
-                                  child: VerticalDivider(
-                                    color: Colors.white,
-                                    thickness: 5,
-                                  ),
-                                ),
-                                Column(
-                                  children: <Widget>[
-                                    Text(documentSnapshot['quantityPerPack'].toString(), style: TextStyle(fontSize: 60)),
-                                    Text("Achivement", style: TextStyle(fontSize: 20)),
+                                    Text("Achivement unlocked", style: TextStyle(fontSize: 20)),
                                   ],
                                 ),
                               ],
@@ -153,8 +271,70 @@ class _MyHomePageState extends State<MyHomePage> {
                               children: <Widget>[
                                 Column(
                                   children: <Widget>[
-                                    Text(documentSnapshot['quitSmokingDate'].toString(), style: TextStyle(fontSize: 60)),
-                                    Text("quitSmokingDate", style: TextStyle(fontSize: 20)),
+                                    Text('$days:$hours:$minutes:$seconds', style: TextStyle(fontSize: 60)),
+                                    Text("    Days  :    Hours :    Minutes :  Seconds", style: TextStyle(fontSize: 22)),
+                                    Text("Total Stop Smoking", style: TextStyle(fontSize: 20)),
+                                  ],
+                                ),
+                              ],
+                            ),
+                          ),
+                        ),
+                        Container(
+                          color: Colors.green,
+                          child: Padding(
+                            padding: const EdgeInsets.only(top: 12.0, bottom: 12.0),
+
+                            child: Row(
+                              mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                              children: <Widget>[
+                                Column(
+                                  children: <Widget>[
+                                    ElevatedButton(
+                                      style: ElevatedButton.styleFrom(elevation: 2),
+                                      onPressed: stopTimer,
+                                      child: Text('Reset', style: TextStyle(fontSize: 18)),
+                                    ),
+                                    ElevatedButton(
+                                      style: ElevatedButton.styleFrom(elevation: 2),
+                                      onPressed: () async{
+                                        final weburl="Total Stop Smoking : $days days $hours hours $minutes Minutes $seconds Seconds";
+                                        Share.share('${weburl}');
+                                      },
+                                      child: Text('Share', style: TextStyle(fontSize: 18)),
+                                    ),
+                                    ],
+                                ),
+                              ],
+                            ),
+                          ),
+                        ),
+                        SizedBox(height: 15),
+                        ListTile(
+                            tileColor: Colors.orange,
+                            title: Text('Pro Tips' ,style: TextStyle(fontSize: 30)),
+                            trailing: const Icon(Icons.share),
+                            subtitle: Text('Connect with a family member, friend or support group member for help in your effort to resist a tobacco craving. Chat on the phone, go for a walk, share a few laughs, or meet to talk and support each other. Counseling can be helpful too.'),
+                            onTap: () => Navigator.of(context).pushReplacement(MaterialPageRoute(builder: (_)=>Protip())),
+                        ),
+                        Container(
+                          color: Colors.orange,
+                          child: Padding(
+                            padding: const EdgeInsets.only(top: 12.0, bottom: 12.0),
+
+                            child: Row(
+                              mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                              children: <Widget>[
+                                Column(
+                                  children: <Widget>[
+                                    ElevatedButton(
+                                      style: ElevatedButton.styleFrom(elevation: 2),
+                                      onPressed: () async{
+                                        final weburl="Connect with a family member, friend or support group member for help in your effort to resist a tobacco craving. Chat on the phone, go for a walk, share a few laughs, or meet to talk and support each other. Counseling can be helpful too.";
+                                        Share.share('${weburl}');
+                                      },
+                                      child: Text('Share', style: TextStyle(fontSize: 18)),
+                                    ),
                                   ],
                                 ),
                               ],
@@ -164,40 +344,21 @@ class _MyHomePageState extends State<MyHomePage> {
 
                         SizedBox(height: 15),
                         ListTile(
-                            tileColor: Colors.yellow,
-                            title: Text('Achievement'),
-                            subtitle: Text('5'),
-                            leading: const Icon(Icons.favorite),
-                            onTap: () => print("ListTile")
-                        ),
-
-                        SizedBox(height: 15),
-                        ListTile(
-                            tileColor: Colors.orange,
-                            title: Text('Pro Tips'),
-                            trailing: const Icon(Icons.share),
-                            subtitle: Text('blablablablablablbalbalbab'),
-                            onTap: () => print("ListTile")
-                        ),
-
-
-                        SizedBox(height: 15),
-                        ListTile(
                             tileColor: Colors.purpleAccent,
-                            title: Text('Community'),
+                            title: Text('Community' ,style: TextStyle(fontSize: 20)),
                             leading: const Icon(Icons.person),
-                            subtitle: Text('blablablablablablbalbalbab'),
-                            onTap: () => print("ListTile")
+                            //onTap: () => Navigator.of(context).pushReplacement(MaterialPageRoute(builder: (_)=>Chats()))
                         ),
-
 
                         SizedBox(height: 15),
-                        ListTile(
-                            tileColor: Colors.grey,
-                            title: Text('Google Ads'),
-                            subtitle: Text('blablablablablablbalbalbab'),
-                            onTap: () => print("ListTile")
-                        ),
+
+                        // Container(
+                        //   alignment: Alignment.center,
+                        //   child: AdWidget(ad:myBanner),
+                        //    width: 800,
+                        //    height:50,
+                        // ),
+
                       ]),
                 );
 
